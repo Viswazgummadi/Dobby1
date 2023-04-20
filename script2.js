85ed80ed1027ce14b357c8e49daf0061a433c23d
Viswaz Gummadi <125357256+Viswazgummadi@users.noreply.github.com>
2023-04-21 00:07:53 +0530

    Add files via upload

--- script2.js
+++ script2.js
@@ -66,3 +66,10 @@ function animateCircles() {
 }
 
 animateCircles();
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
